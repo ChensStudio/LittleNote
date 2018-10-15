ef06b004d7c547fd9e70205b2f70de25dd2a88b3
Yang Chen <young.y.chen@gmail.com>
2018-10-15 10:25:37 -0700

    change environment configuration

--- imports/api/accounts/server/publications.js
+++ imports/api/accounts/server/publications.js
@@ -4,13 +4,8 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from '../accounts.js';
 
 Meteor.publish('accounts', function accounts() {
-    // if (!this.address) {
-    //   return this.ready();
-    // }
-  
     return Accounts.find({
-        // address: this.address,
     }, {
-      fields: Accounts.publicFields,
+        fields: Accounts.publicFields,
     });
-  });
+});

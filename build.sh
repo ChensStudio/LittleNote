421bf4688edbb88e1166b0f3c3f13f41524e23c9
Yang Chen <young.y.chen@gmail.com>
2018-12-10 14:55:17 -0800

    change server

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ do
 case $i in
     -t|--test)
         message="Delivered to TEST."
-        target="yang_chen@35.197.94.126:/home/yang_chen"
+        target="ubuntu@54.186.212.101:/home/ubuntu"
     ;;
     -p|--prod)
         message="Delivered to PROD"
-        target="moac@52.43.43.32:/home/moac"
+        target="ubuntu@54.186.212.101:/home/ubuntu"
     ;;
 esac
 done
@@ -20,7 +20,7 @@ done
 if test ${target} != "exit"
 then
     meteor build --architecture os.linux.x86_64  ../build/LittleNote
-    scp -i /Users/ychen/innowells/pem/gcp ../build/LittleNote/LittleNote.tar.gz ${target}
+    scp -i /Users/ychen/innowells/pem/moacdappwallet.pem ../build/LittleNote/LittleNote.tar.gz ${target}
 fi
 
 echo ${message}

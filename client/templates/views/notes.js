c6b52b22370c8d7bd69a4131663d26aee72b97ea
WeiqiWang <ww15c@qq.com>
2019-03-01 17:53:39 -0800

    try disable brower script

--- client/templates/views/notes.js
+++ client/templates/views/notes.js
@@ -6,6 +6,26 @@ import MoacConnect from '../../moacconnect.js';
 import { type } from 'os';
 // import '../../client.js';
 
+
+// (function() { 
+// try { 
+// var $_console$$ = console; 
+// Object.defineProperty(window, "console", { 
+// get: function() { 
+// if ($_console$$._commandLineAPI) 
+// throw "抱歉, 为了用户安全, 本网站已禁用console脚本功能"; 
+// return $_console$$ 
+// }, 
+// set: function($val$$) { 
+// $_console$$ = $val$$ 
+// } 
+// }) 
+// } catch ($ignore$$) { 
+// } 
+// })(); 
+
+
+
 var notesLoaded = false;
 var accountsLoaded = false;
 var hide = false;
